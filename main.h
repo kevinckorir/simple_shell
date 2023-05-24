4b8c99b998cf6d725c8e4fa7fba1aac4b699b6f0
Kevin Korir <korirckevin@gmail.com>
2023-05-24 15:04:43 +0300

    Added main.h and shell.c

---
+++ main.h
@@ -0,0 +1,14 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <string.h>
+void execute_command(char* command);
+void handle_exit();
+char* parse_command(char* input);
+int search_command(char* command);
+#endif /*MAIN_H*/
---
+++ shell.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <string.h>
+
+#define MAX_COMMAND_LENGTH 100
+
+void execute_command(char* command) {
+    pid_t pid;
+    int status;
+
+    pid = fork();
+    if (pid == -1) {
+        perror("Error: Forking process failed");
+        return;
+    } else if (pid == 0) {
+        char* args[] = {command, NULL};
+        execvp(command, args);
+
+        perror("Error: Command not found");
+        exit(EXIT_FAILURE);
+    } else {
+        waitpid(pid, &status, 0);
+    }
+}
+
+int main() {
+    char command[MAX_COMMAND_LENGTH];
+
+    while (1) {
+        printf("$ ");
+
+        if (fgets(command, sizeof(command), stdin) == NULL) {
+            printf("\nExit\n");
+            break;
+        }
+}
+
+        command[strcspn(command, "\n")] = '\0';
+
+        if (strlen(command) == 0) {
+            continue;
+        }
+
+        execute_command(command);
+    }
+
+    return 0;
+}
+
