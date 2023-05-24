0bc9e6b033aaceb473e71440c8c4dfcde58f09d6
Anuoluwapo25 <Anuoluwapoali25@gmail.com>
2023-05-24 12:56:16 -0700

    ammended

--- _strcspn.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-#include <string.h>
-#include "shell.h"
-
-/**
- * _strcspn - main function
- * @str: string1
- * @r:*/
--- shell.h
+++ shell.h
@@ -8,12 +8,15 @@
 #include <sys/wait.h>
 #include <string.h>
 
+extern char **environ;
+
 void _execute(char *command, char **arguments);
 void handle_exit();
 int my_strlen(char *s);
 char *parse_command(char *input);
 int search_command(char *command);
 void _execve(char *cmd, char **args);
-extern char **environ;
+ssize_t _getline(char **lineptr, size_t *n, FILE *stream);
+
 
 #endif /*SHELL_H*/
